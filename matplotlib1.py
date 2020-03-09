3814ed12b1860dc559b0d6c356b995a9bb1d16d5
sy590 <48459177+sy590@users.noreply.github.com>
2020-03-09 16:42:30 -0400

    2

---
+++ matplotlib1.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+import random
+import matplotlib
+from matplotlib import font_manager
+
+# windows 和 Linux 设置字体的方法
+font = {"family": 'MicroSoft YaHei',
+        'weight': 'bold',
+        'size': 7}
+matplotlib.rc("font", **font)
+matplotlib.rc("font", family='MicroSoft YaHei', weight="bold")
+
+
+# my_font = font_manager.FontPorperties(fname = "\AppData\Local\Microsoft\FontCache\4\CloudFonts\TTC")
+
+x = range(0, 120)
+y = [random.randint(20, 35) for i in range(120)]
+
+plt.figure(figsize=(20, 8), dpi=80)
+plt.plot(x, y)
+
+# 调整X轴的刻度
+
+_x = list(x)
+_xtick_labels = ["10点,{}分".format(i) for i in range(60)]
+_xtick_labels += ["11 点,{}分".format(i) for i in range(60)]
+# 取步长，数字和字符串一一对应，数据的长度一样
+plt.xticks(_x[::3], _xtick_labels[::3], rotation=90)  # FontProperties=my_font) #ratation旋转的度数
+plt.show()
